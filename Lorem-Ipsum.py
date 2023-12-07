0cc39917878cfd02c4512000006acf3f3d99dcdc
Tucnuc <cer42372@spseol.cz>
2023-12-07 19:34:26 +0100

    Release 2.0

--- Lorem-Ipsum.py
+++ Lorem-Ipsum.py
@@ -1,6 +1,7 @@
 import random
 import time
 import textwrap
+import os
 
 nevim = []
 
@@ -13,6 +14,7 @@ vsechny_odstavce = []
 ostrava = ("rožnout","chachar","pyčo","grcat","bo","bulat","hrča","najebat","kobzole","kaj","včil","chuj","cyp","bazaly","roba","šufan","kajsyk","pazury","štrample","štrample","kura","robit","dupa","mňamka","šupina","pivoň","fofr")
 brno = ("pindy","čapy","budka","bagrovat","šalina","špilas","bazmek","krýgl","merka","brajgl","dovalit","békať","svišť","vyfičet","šméčko","čubrnět","špica","róry","hrnót","krtek","slintáč","hokna","rychna","betla","šlohnót")
 hana = ("cecan","ošmóranec","ohonit","šošeň","otěkat","šak","šiblé","šklibit","šléšky","šmédit","šmodrcha","špičák","šršňák","orál","čapa","řbet","čuňa","bék","brňák","brikule","céch","loskanec","máčka","masenko","mrčet","nigde")
+ostatni = ('dolor','sit','amet','consectetur','adipiscing','elit','sed','do','eiusmod','tempor','incididunt','ut','labore','et','dolore','magna','aliqua','ut','enim','ad','minim','veniam','quis','nostrud','exercitation','ullamco','laboris','nisi')
 nareci = ostrava
 
 zapnuti = input("Zmáčkněte Enter pro zapnutí: ")
@@ -75,26 +77,53 @@ while True:
     if "magic" in nevim:
         for _ in range(odstavce):
             vybrane_slova = [slovo for slovo in nareci if len(slovo) <= max_delka]
+            vybrana_matlanina = [slovo for slovo in ostatni if len(slovo) <= max_delka]
             final_slova = []
             for _ in range(delka_odstavce):
                 cislo = random.randint(0,20)
                 cislo2 = random.randint(0,12)
+                cislo3 = random.randint(0,2)
                 losovane_slova = random.choice(vybrane_slova)
+                if cislo3 == 1:
+                    losovane_slova = random.choice(vybrana_matlanina)
                 if cislo == 3 and cislo2 != 4:
                     losovane_slova += ","
                 if cislo2 == 4 and cislo != 3:
                     losovane_slova += "."
                 final_slova.append(losovane_slova)
-
             final_text = textwrap.fill(' '.join(final_slova), width=delka_radku)
             final_text = final_text.capitalize()
             final_text += "."
             vsechny_odstavce.append(final_text)
             alfa_text = "\n\n".join(vsechny_odstavce)
 
-        beta_text = alfa_text
+        gamma_text = ""
+        tecka = 0
+        for T in alfa_text:
+            if tecka < 1:
+                gamma_text += T
+            else:
+                gamma_text += T.upper()
+            if T == ".":
+                tecka = 2
+            else:
+                tecka += -1
+
+        beta_text = gamma_text
         beta_text += "\n\n\033[3mVytvořeno \033[1mLorem Ipsum Generátorem 3000 DeLuxe++\033[0m\033[0m"
+        print("Generuji...")
+        print("")
+        time.sleep(1)
         print(beta_text)
         with open('vystup.txt', 'w') as f:
-            f.write(alfa_text)
-        break
+            f.write(gamma_text)
+
+        time.sleep(2)
+        print("")
+        open = int(input("Chcete otevřít textový dokument s vygenerovaným textem? [1 - ano, 2 - ne]: "))
+        if open == 1:
+            time.sleep(0.2)
+            os.system("notepad.exe vystup.txt")
+            break
+        else:
+            break
